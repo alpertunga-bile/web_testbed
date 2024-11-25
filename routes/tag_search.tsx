2baa502324ae0a2525960b12088c625b0808a571
alpertunga-bile <holy_celestine@hotmail.com>
2024-11-25 14:42:42 +0300

    tag search page is added

--- routes/tag_search.tsx
+++ routes/tag_search.tsx
@@ -1,6 +1,21 @@
-import { Handlers } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import Trie from "../static/trie.ts";
+import { get_tags_by_char } from "../static/utilities.ts";
 
 export const handler: Handlers = {
-  GET(_req, ctx) {
+  async GET(_req, ctx) {
+    const a_tags: Set<string> = await get_tags_by_char("a");
+    const tag_trie: Trie = new Trie(a_tags);
+
+    if (tag_trie.contains("as")) {
+      const vars = tag_trie.starts_with("as");
+      console.log(vars);
+    }
+
+    return ctx.render();
   },
 };
+
+export default function TagSearchPage(props: PageProps) {
+  return <p>Hello World</p>;
+}
