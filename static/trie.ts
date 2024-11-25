320f20fd6616b8a9c5d2cffcff02a2b1488e9a97
alpertunga-bile <holy_celestine@hotmail.com>
2024-11-25 14:42:16 +0300

    search func is added

--- static/trie.ts
+++ static/trie.ts
@@ -3,17 +3,25 @@ class TrieNode {
     this.key_node = is_key_node;
   }
 
+  contains(char: string): boolean {
+    return this.nodes.has(char);
+  }
+
+  get(char: string): TrieNode | undefined {
+    return this.nodes.get(char);
+  }
+
   key_node: boolean = false;
   readonly nodes: Map<string, TrieNode> = new Map<string, TrieNode>();
 }
 
 class Trie {
-  constructor(values?: string[]) {
+  constructor(values?: Iterable<string>) {
     if (!values) {
       return;
     }
 
-    values.forEach((value) => this.insert(value));
+    this.multi_insert(values);
   }
 
   contains(val: string): boolean {
@@ -25,7 +33,7 @@ class Trie {
         return false;
       }
 
-      if (!current_node.nodes.has(char)) {
+      if (!current_node.contains(char)) {
         return false;
       }
 
@@ -33,6 +41,12 @@ class Trie {
     }
 
     return true;
+  }
+
+  multi_insert(values: Iterable<string>) {
+    for (const value of values) {
+      this.insert(value);
+    }
   }
 
   insert(val: string) {
@@ -45,13 +59,13 @@ class Trie {
       }
 
       if (current_node.nodes.has(char)) {
-        current_node = current_node.nodes.get(char);
+        current_node = current_node.get(char);
         continue;
       }
 
       const new_node = new TrieNode(false);
       current_node.nodes.set(char, new_node);
-      current_node = current_node.nodes.get(char);
+      current_node = current_node.get(char);
     }
 
     if (current_node) {
@@ -59,7 +73,62 @@ class Trie {
     }
   }
 
+  private get_last_node(value: string): TrieNode | undefined {
+    let current_node: TrieNode | undefined = this.root;
+    const chars = value.split("");
+
+    for (const char of chars) {
+      if (!current_node) {
+        return undefined;
+      }
+
+      if (!current_node.contains(char)) {
+        return undefined;
+      }
+
+      current_node = current_node.get(char);
+    }
+
+    return current_node;
+  }
+
+  private inner_starts_with(
+    node: TrieNode | undefined,
+    prefix: string,
+  ): string[] {
+    if (!node) {
+      return [];
+    }
+
+    const keys = [];
+
+    if (node.key_node) {
+      keys.push(prefix);
+    }
+
+    node.nodes.forEach((value_node, key_char) => {
+      keys.push(
+        ...this.inner_starts_with(
+          value_node,
+          prefix + key_char,
+        ),
+      );
+    });
+
+    return keys;
+  }
+
+  starts_with(prefix: string): string[] {
+    const last_node: TrieNode | undefined = this.get_last_node(prefix);
+
+    if (!last_node) {
+      return [];
+    }
+
+    return this.inner_starts_with(last_node, prefix);
+  }
+
   readonly root: TrieNode = new TrieNode(false);
 }
 
-export const tag_trie: Trie = new Trie();
+export default Trie;
