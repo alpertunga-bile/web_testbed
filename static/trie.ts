a6fef4f216b72a276dc87dc6f09469676c9e5df1
alpertunga-bile <holy_celestine@hotmail.com>
2024-11-25 11:09:50 +0300

    trie class is added

--- static/trie.ts
+++ static/trie.ts
@@ -7,14 +7,32 @@ class TrieNode {
   readonly nodes: Map<string, TrieNode> = new Map<string, TrieNode>();
 }
 
-export class Trie {
-  constructor(values: string[]) {
+class Trie {
+  constructor(values?: string[]) {
+    if (!values) {
+      return;
+    }
+
     values.forEach((value) => this.insert(value));
   }
 
-  contains(val: string) {
+  contains(val: string): boolean {
     const chars = val.split("");
     let current_node: TrieNode | undefined = this.root;
+
+    for (const char of chars) {
+      if (!current_node) {
+        return false;
+      }
+
+      if (!current_node.nodes.has(char)) {
+        return false;
+      }
+
+      current_node = current_node.nodes.get(char);
+    }
+
+    return true;
   }
 
   insert(val: string) {
@@ -43,3 +61,5 @@ export class Trie {
 
   readonly root: TrieNode = new TrieNode(false);
 }
+
+export const tag_trie: Trie = new Trie();
