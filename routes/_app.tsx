2d6645018fcb75cd82bc26a84000f55f9c17a546
alpertunga-bile <holy_celestine@hotmail.com>
2024-11-25 16:10:19 +0300

    pico css is added

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,9 +4,15 @@ export default function App({ Component }: PageProps) {
     <html>
       <head>
         <meta charset="utf-8" />
+        <meta name={"color-scheme"} content={"light dark"} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>web_testbed</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link
+          rel="stylesheet"
+          href="/pico.classless.conditional.fuchsia.min.css"
+        >
+        </link>
       </head>
       <body>
         <Component />
