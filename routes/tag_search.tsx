e8825b0d62d4b8d721d69d4980dd15f88e7861bd
alpertunga-bile <holy_celestine@hotmail.com>
2024-11-26 10:56:19 +0300

    interface and date funcs are added

--- routes/tag_search.tsx
+++ routes/tag_search.tsx
@@ -7,30 +7,75 @@ import Trie from "../static/trie.ts";
 import { get_tags_by_char } from "../static/utilities.ts";
 import { decode, encode } from "cbor-x";
 
+import { difference, format, parse } from "@std/datetime";
+
 const tag_head_chars = "abcdefghijklmnopqrstuvwxyz123456789".split("");
 const tags_binary_filepath = "./static/tags.bin";
+const date_formating = "dd-MM-yyyy";
 
 interface ITagSearchData {
   tag_trie: Trie;
 }
 
+interface ITagInfo {
+  creation_time: string;
+  remaining_day: number;
+  tags: Array<Array<string>>;
+}
+
+async function create_cache_file(tag_sets: Set<string>[]) {
+  const tag_info: ITagInfo = {
+    creation_time: format(new Date(), date_formating),
+    remaining_day: 5,
+    tags: [],
+  };
+
+  tag_info.tags = tag_sets.map((set_value) => Array.from(set_value));
+
+  const json_array = new Uint8Array(encode(JSON.stringify(tag_info)));
+
+  await Deno.writeFile(tags_binary_filepath, json_array);
+}
+
+function check_if_cache_expired(
+  created_time: string | undefined,
+  expire_day: number = 5,
+): boolean {
+  if (!created_time) {
+    return false;
+  }
+
+  const created_time_date = parse(created_time, date_formating);
+  const current_time_date = new Date();
+
+  const day_between = difference(created_time_date, current_time_date).days ||
+    0;
+
+  return day_between >= expire_day;
+}
+
 export const handler: Handlers<ITagSearchData> = {
   async GET(_req, ctx) {
-    const tag_promises: Promise<Set<string>>[] = [];
     const tag_trie: Trie = new Trie();
 
     const is_bin_exists = await exists(tags_binary_filepath);
 
     if (is_bin_exists) {
       const data = await Deno.readFile(tags_binary_filepath);
-      const json_map: Map<string, Array<string>> = new Map(
-        Object.entries(JSON.parse(decode(data))),
-      );
+      const tag_info: ITagInfo = JSON.parse(decode(data));
 
-      json_map.forEach((tags) => tag_trie.multi_insert(tags));
+      if (
+        !check_if_cache_expired(tag_info.creation_time, tag_info.remaining_day)
+      ) {
+        tag_info.tags.forEach((tags) => tag_trie.multi_insert(tags));
 
-      return ctx.render({ tag_trie: tag_trie });
+        return ctx.render({ tag_trie: tag_trie });
+      }
+
+      await Deno.remove(tags_binary_filepath);
     }
+
+    const tag_promises: Promise<Set<string>>[] = [];
 
     tag_head_chars.forEach((char) =>
       tag_promises.push(get_tags_by_char(char, 5))
@@ -38,19 +83,9 @@ export const handler: Handlers<ITagSearchData> = {
 
     const tag_sets = await Promise.all(tag_promises);
 
-    const tags: Map<string, Array<string>> = new Map<string, Array<string>>();
-
-    tag_sets.forEach((set_value, index) => {
-      tags.set(tag_head_chars[index], Array.from(set_value));
-    });
-
-    const tag_object = Object.fromEntries(tags);
-
-    const json_array = new Uint8Array(encode(JSON.stringify(tag_object)));
-
-    await Deno.writeFile(tags_binary_filepath, json_array);
-
     tag_sets.forEach((set_value) => tag_trie.multi_insert(set_value));
+
+    await create_cache_file(tag_sets);
 
     return ctx.render({ tag_trie: tag_trie });
   },
