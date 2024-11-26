552fd80872f39e649e527bae502f256c3ceddd2f
alpertunga-bile <holy_celestine@hotmail.com>
2024-11-26 15:39:31 +0300

    input suggestion is added

--- islands/SuggestedSearchbar.tsx
+++ islands/SuggestedSearchbar.tsx
@@ -1,4 +1,4 @@
-import { Ref, useEffect, useRef } from "preact/hooks";
+import { useEffect } from "preact/hooks";
 import PicoStyle from "../components/PicoStyle.tsx";
 import Trie from "../static/trie.ts";
 import { useSignal } from "@preact/signals";
@@ -6,12 +6,12 @@ import { useSignal } from "@preact/signals";
 export default function SuggestedSearchbar(
   props: { tags: Array<Array<string>> },
 ) {
-  const suggestion_ref: Ref<HTMLUListElement> = useRef(null);
   const tag_obj = useSignal(new Trie());
+  const suggestions: Signal<string[]> = useSignal([]);
 
   useEffect(() => {
     props.tags.forEach((tag) => tag_obj.value.multi_insert(tag));
-  });
+  }, []);
 
   const searchbar_oninput = (
     event,
@@ -19,27 +19,17 @@ export default function SuggestedSearchbar(
     const input_value: string = event.target.value;
 
     if (input_value === "") {
+      suggestions.value = [];
       return;
     }
 
-    const children_count = suggestion_ref.current?.children.length || 0;
+    suggestions.value = [...tag_obj.value.starts_with(input_value).slice(0, 5)];
 
-    for (let i = 0; i < children_count; ++i) {
-      suggestion_ref.current?.removeChild(
-        suggestion_ref.current.children[i],
-      );
+    if (
+      suggestions.value.length === 1 && suggestions.value[0] === input_value
+    ) {
+      suggestions.value = [];
     }
-
-    const suggestions = tag_obj.value.starts_with(input_value).slice(0, 10);
-
-    console.log(suggestions);
-
-    suggestions.forEach((suggestion) => {
-      const li_elem = new HTMLLIElement();
-      li_elem.innerText = suggestion;
-
-      suggestion_ref.current?.appendChild<HTMLLIElement>(li_elem);
-    });
   };
 
   return (
@@ -49,10 +39,14 @@ export default function SuggestedSearchbar(
         type={"search"}
         placeholder={"Search a tag"}
         aria-label={"Search"}
-        value=""
         onInput={searchbar_oninput}
       />
-      <ul ref={suggestion_ref}></ul>
+
+      <ul key="searchbar_suggestion">
+        {suggestions.value.map((suggestion) => (
+          <li key={`tag_${suggestion}`}>{suggestion}</li>
+        ))}
+      </ul>
     </PicoStyle>
   );
 }
