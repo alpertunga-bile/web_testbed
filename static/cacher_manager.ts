30893484eca4771fcd7e8db4b7a8362fee3d2533
alpertunga-bile <holy_celestine@hotmail.com>
2024-12-01 15:54:22 +0300

    cacher manager funcs are declared

--- static/cacher_manager.ts
+++ static/cacher_manager.ts
@@ -1,6 +1,6 @@
-import { Cacher } from "./cacher.ts";
+import { existsSync } from "$std/fs/exists.ts";
+import { Cacher, CacherReturnInfo } from "./cacher.ts";
 import { CacherCompression, CacherDateRemainingUnit } from "./cacher.ts";
-import { LRUCache } from "./lru_cache.ts";
 import * as crypto from "jsr:@std/crypto";
 import * as path from "jsr:@std/path";
 
@@ -22,8 +22,72 @@ export const default_cacher_manager_options: CacherManagerOptions = {
   hash_filename: true,
 };
 
-export class CacherManager<ContentType> {
+export type CacherManagerReturnInfo = CacherReturnInfo;
+
+class CacherLRUCache extends Map<string, string> {
+  constructor(max_size: number = 128) {
+    super();
+
+    this.max_size = max_size;
+  }
+
+  private update_most_recent(key: string, value: string) {
+    super.delete(key);
+    super.set(key, value);
+  }
+
+  private check_if_full(): string | undefined {
+    if (this.size <= this.max_size) {
+      return undefined;
+    }
+
+    const key = this.keys().next().value!;
+    const value = this.get(key);
+
+    this.delete(key);
+
+    return value;
+  }
+
+  override has(key: string): boolean {
+    const is_exists: boolean = super.has(key);
+
+    if (is_exists) {
+      this.update_most_recent(key, super.get(key)!);
+    }
+
+    return is_exists;
+  }
+
+  override get(key: string): string | undefined {
+    if (!super.has(key)) {
+      return undefined;
+    }
+
+    const value: string = super.get(key)!;
+    this.update_most_recent(key, value);
+
+    return value;
+  }
+
+  override set(key: string, value: string): this {
+    this.update_most_recent(key, value);
+    const removed_value: string | undefined = this.check_if_full();
+
+    removed_value && Deno.removeSync(removed_value);
+
+    return this;
+  }
+
+  private readonly max_size;
+}
+
+export class CacherManager {
   constructor(options: CacherManagerOptions = default_cacher_manager_options) {
+    if (!existsSync(options.save_path)) {
+      Deno.mkdirSync(options.save_path);
+    }
+
     this.cacher = new Cacher({
       save_path: options.save_path,
       compression_type: options.compression_type,
@@ -31,7 +95,7 @@ export class CacherManager<ContentType> {
       remaining_time: options.remaining_time,
     });
 
-    this.file_map = new LRUCache(options.max_cache_file);
+    this.file_map = new CacherLRUCache(options.max_cache_file);
 
     this.get_cache_filepath = (filename: string) => {
       return path.join(options.save_path, filename);
@@ -60,17 +124,64 @@ export class CacherManager<ContentType> {
   ): Promise<void> {
     const cache_filename = this.get_hashed_filename(filename);
 
-    if (overwrite && this.file_map.has(cache_filename)) {
+    if (!overwrite && this.file_map.has(cache_filename)) {
       return;
     }
 
-    this.file_map.set(filename, cache_filename);
+    this.file_map.set(filename, this.get_cache_filepath(filename));
 
     await this.cacher.from_str(value, cache_filename, overwrite);
   }
 
+  async save_from_obj(
+    value: object,
+    filename: string,
+    overwrite: boolean = true,
+  ) {
+    const cache_filename = this.get_hashed_filename(filename);
+
+    if (!overwrite && this.file_map.has(cache_filename)) {
+      return;
+    }
+
+    this.file_map.set(filename, this.get_cache_filepath(filename));
+
+    await this.cacher.from_obj(value, cache_filename, overwrite);
+  }
+
+  async save_from_map<K, V>(
+    value: Map<K, V>,
+    filename: string,
+    overwrite: boolean = true,
+  ) {
+    const cache_filename = this.get_hashed_filename(filename);
+
+    if (!overwrite && this.file_map.has(cache_filename)) {
+      return;
+    }
+
+    this.file_map.set(filename, this.get_cache_filepath(filename));
+
+    await this.cacher.from_map(value, cache_filename, overwrite);
+  }
+
+  async get_data(filename: string): Promise<CacherManagerReturnInfo> {
+    const cache_filename = this.get_hashed_filename(filename);
+
+    const cacher_return_info: CacherManagerReturnInfo = await this.cacher
+      .get_data(
+        cache_filename,
+      );
+
+    return cacher_return_info;
+  }
+
+  is_cache_exists(filename: string): boolean {
+    return this.file_map.has(filename);
+  }
+
   private readonly get_cache_filepath: (filename: string) => string;
   private readonly get_hashed_filename: (filename: string) => string;
-  private readonly file_map: LRUCache<string, string>;
+  private readonly file_map: CacherLRUCache;
   private readonly cacher: Cacher;
 }
